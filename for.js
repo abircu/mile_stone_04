4ca51e47da706610d6c3a5aa5e60fbbf04f6740d
abircu <mdabircse4873@gmail.com>
2023-07-31 22:43:02 +0600

    loop practice

---
+++ for.js
@@ -0,0 +1,5 @@
+var numbers = [20, 23, 50, 60, 22, 10, 11];
+for (var i = 0; i <= numbers.length; i++) {
+  var number = numbers[i];
+  console.log(number);
+}
---
+++ loop.js
@@ -0,0 +1,6 @@
+// while loop
+var rostGiven = 1;
+while (rostGiven < 100) {
+  rostGiven = rostGiven + 2;
+  console.log(rostGiven);
+}

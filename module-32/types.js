6ae86e306aee37cb3745f2572459c5e70bfc1a36
abircu <mdabircse4873@gmail.com>
2023-08-27 00:02:57 +0600

    types check

--- module-32/types.js
+++ module-32/types.js
@@ -2,4 +2,14 @@ const a = 5;
 const b = "kdkdk";
 const student = { id: 32, class: 7 };
 const ages = [10, 12, 13, 25, 23];
-console.log(typeof a, typeof b, typeof ages, typeof student);
+
+let x = 5;
+let y = x;
+console.log(x, y);
+y = 7;
+console.log(x, y);
+let p = { job: "web devoloper" };
+let q = p;
+// non primitive partially change
+q.job = "front end developer";
+console.log(p, q);
